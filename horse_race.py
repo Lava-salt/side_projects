ce8cdfc12bdfb4fa955afc3a085e296d8ae342d1
Can Koyuncu <reyyadvan@gmail.com>
2025-07-05 22:46:17 +0300

    Create horse_race.py

---
+++ horse_race.py
@@ -0,0 +1,54 @@
+# Horse race simulation, but after each race, their winning chances change.
+from random import randint
+from time import sleep
+from os import system
+print("Welcome to the Horse Race Simulator!")
+print("If you think this just prints a random horse name, you're wrong.")
+print("Because if they win, the chance of it winning increases.")
+print("Long later, only one horse will be able to win.")
+horses = [
+    "White",
+    "Green",
+    "Peacock",
+    "Plum",
+    "Scarlet",
+    "Mustard",
+    "Orchid",
+    "Violet",
+    "Azure",
+    "Cyan"
+]
+chances = [10] * 10
+interval = float(input("Interval between races (seconds): "))
+matches = horses.copy()
+debug = 0
+while True:
+    for i in range(10):
+        print(f"Horse {i + 1}#: {horses[i]} - {chances[i]}% Win Chance")
+    print("\nGraph of win chances:")
+    print("Up/Down: Chance of Winning")
+    print("Left/Right: Horse No.")
+    while True:
+        for i in range(10):
+            big = 100
+            if randint(0, big) <= chances[i]:
+                print(f"{horses[i]} won the latest race.")
+                winner = horses[i]
+                break
+            else:
+                big -= chances[i]
+        try:
+            debug = winner
+            break
+        except Exception:
+            continue
+    matches.append(winner)
+    for i in range(10):
+        percentage = 0
+        for j in matches:
+            if j == horses[i]:
+                percentage += 1
+        percentage = 100 / len(matches) * percentage
+        chances[i] = percentage
+    sleep(interval)
+    system("cls")
